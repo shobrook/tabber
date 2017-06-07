31f03813e9b4c1f9b3418167dd74f469439cfbb5
Jonathan Shobrook <shobrookj@gmail.com>
2017-06-06 23:33:43 -0400

    Tweaks 💅
    - Separated sent from received messages
    - Removed <span>s from message contents
    - Added max z-index and crosshair cursor

--- content.js
+++ content.js
@@ -1,34 +1,36 @@
 ////GLOBALS////
 
-/* Nothin' here yet */
+injected = false;
 
 ////MAIN////
 
-console.log("Initializing messagebook.");
-window.localStorage.setItem('messagebook-id', chrome.runtime.id);
-console.log("Extension ID: " + window.localStorage.getItem('messagebook-id'));
+// TODO: Refactor so that when user clicks button again, they can select more messages but injection doesn't occur
+
+console.log("Initializing tabber.");
+window.localStorage.setItem('tabber-id', chrome.runtime.id);
+console.log("Extension ID: " + window.localStorage.getItem('tabber-id'));
 
 var payload = function() {
 
-	var scrapeMessages = function() {
-	
+	var scrapeAllMessages = function() {
+
 		var scrapedMessages = []; // All loaded messages and their respective sender + coordinates, in chronological order
-		
+
 		var containerNode = document.getElementsByClassName('__i_')[0];
 		containerNode.childNodes.forEach(function(child) {
-	
+
 			if (child.tagName == 'DIV' && child.id.length > 0) {
 				child.childNodes.forEach(function(c) {
-	
+
 					if (c.tagName == 'DIV') {
 						var msgWrapperNodes = c.childNodes[0].getElementsByClassName('clearfix');
-	
+
 						for (var i = 0; i < msgWrapperNodes.length; i++) {
 							var msgNode = msgWrapperNodes[i].childNodes[0].childNodes[0];
-							
+
 							// Detects if message is rich media content
 							if (msgNode == undefined || msgNode == null) continue;
-		
+
 							var messageContents = msgNode.textContent;
 							var sender = 0; // 0 for sent, 1 for received
 							if (window.getComputedStyle(msgWrapperNodes[i].childNodes[0], null).getPropertyValue("background-color") == "rgb(241, 240, 240)") {
@@ -39,125 +41,152 @@ var payload = function() {
 							scrapedMessages.push({"sender": sender, "message": messageContents, "coordinates": [position.left + window.pageXOffset, position.top + window.pageYOffset]});
 						}
 					}
-	
 				});
 			}
-
 		});
-		
 		return scrapedMessages;
-	
 	}
 
-	var selectMessages = function() {
-		
+	var selectMessages = function(callback) {
+
 		var region = {"initial": [], "final": []}; // Coordinate bounds of selection region
 		var messages = []; // All loaded messages in the form [{"sender": sender, "message": messageContents, "coordinates": [x, y]}, ...]
+		var tabber_svg, tabber_mask, tabber_clip; // Masking animation shared variables
 
-		var onMouseDown = function(e) {
+		var initSVG = function() {
+			console.log("initSVG");
+			// NOTE: Change this to change where mask is placed
+			var mask_target = document.body; // document.body for whole page
 
-			region.initial = [e.pageX, e.pageY]; // Pushes initial coordinates to region array
-	
-			// TO-DO: Create a selection div using clip-path somehow (good luck @Matthew)
-			// TO-DO: Update width and height of selection div as mouse moves
-	
+			var svg_defs = `<svg id="tabber_svg" width="100%" height="100%">
+							<defs>
+								<mask id="Mask">
+									<rect width="100%" height="100%" fill="#fff" />
+									<rect id="clip" x="0" y="0" width="0" height="0" fill="#000"></rect>
+								</mask>
+							</defs>
+							<rect id="mask" x="0" y="0" width="100%" height="100%" fill="black" opacity =".5" mask="url(#Mask)"/>
+							</svg>`;
+
+			$(mask_target).insertAdjacentHTML("beforeend", svg_defs);
+			tabber_svg = document.getElementById("tabber_svg");
+			tabber_svg.style.position = "fixed";
+			tabber_svg.style.top = "0";
+			tabber_svg.style.left = "0";
+			tabber_svg.style.zIndex = "2147483647";
+			tabber_svg.style.cursor = "crosshair";
+
+			tabber_mask = document.getElementById("mask");
+			tabber_clip = document.getElementById("clip");
+
+			// Allows user to interact with mask
+			tabber_mask.addEventListener("mousedown", startMask, false);
+			tabber_mask.addEventListener("mouseup", endMask, false);
 		}
-	
-		var toggle = false;
-		var onMouseUp = function(e) {
-	
-			region.final = [e.pageX, e.pageY]; // Pushes final coordinates to region array
 
-			// TO-DO: Remove selection div
-	
-			messages = scrapeMessages();
-			toggle = true;
-	
+		var startMask = function(e) {
+
+			region.initial = [e.pageX, e.pageY]; // Records initial coordinates in region array
+			// Creates "hole" in mask and attaches resize listener
+			tabber_clip.setAttribute("x", e.pageX);
+			tabber_clip.setAttribute("y", e.pageY);
+			tabber_clip.setAttribute("width", "0");
+			tabber_clip.setAttribute("height", "0");
+			tabber_mask.addEventListener("mousemove", resizeMask, false);
 		}
-	
-		var div = document.createElement('div'); div.id = "wrapper";
-		div.style = "cursor: crosshair; background-color: rgba(78,84,91,.35); z-index: 2147483647; width: 100%; height: 100%; top: 0px; left: 0px; display: block; position: absolute;";
-		div.addEventListener("mousedown", function(e) { onMouseDown(e); }, false);
-		div.addEventListener("mouseup", function(e) { onMouseUp(e); }, false);
-		document.body.appendChild(div); // Imposes a low-opacity "canvas" on entire page
+
+		var resizeMask = function(e) {
+			// x
+			if (e.pageX - region.initial[0] > 0) tabber_clip.setAttribute("width", e.pageX - region.initial[0]);
+			else {
+				tabber_clip.setAttribute("x", e.pageX);
+				tabber_clip.setAttribute("width", region.initial[0] - e.pageX);
+			}
+			// y
+			if (e.pageY - region.initial[1] > 0) tabber_clip.setAttribute("height", e.pageY - region.initial[1]);
+			else {
+				tabber_clip.setAttribute("y", e.pageY);
+				tabber_clip.setAttribute("height", region.initial[1] - e.pageY);
+			}
+		}
+
+		var endMask = function(e) {
+			region.final = [e.pageX, e.pageY]; // Records final coordinates in region array
+			tabber_mask.removeEventListener("mousemove", resizeMask, false);
+
+			// NOTE: Naive cleanup. More to do based on how we handle user actions
+			// tabber_mask.parentNode.removeChild(tabber_mask);
+			// tabber_clip.parentNode.removeChild(tabber_clip);
+			tabber_svg.parentNode.removeChild(tabber_svg);
+
+			filterMessages();
+		}
+
+		initSVG();
+
+		messages = scrapeAllMessages();
 
 		var selectedMessages = []; // Selected messages distinguished by sender (ordered chronologically)
 
-		// Testing purposes only (TO-DO: Call the below code once toggle is true)
-		setTimeout(function() {
+		var filterMessages = function() {
 
-			if (toggle) {
-				var child = document.getElementById("wrapper");
-				document.body.removeChild(child);
-			
-				// Filter messages through region bounds and append to selectedMessages
-				messages.forEach(function(m) {
+			// Filter messages through region bounds and append to selectedMessages
+			// TODO: Should we grab messages that are partially within mask? Only works if message's top left is inside
+			messages.forEach(function(m) {
 
-					if (region.initial[0] < region.final[0] && region.initial[1] < region.final[1]) {
-						if ((m.coordinates[0] >= region.initial[0] && m.coordinates[0] <= region.final[0]) &&
-							(m.coordinates[1] >= region.initial[1] && m.coordinates[1] <= region.final[1])) {
-							selectedMessages.push(m.message);
-						}
-					} else if (region.initial[0] < region.final[0] && region.initial[1] > region.final[1]) {
-						if ((m.coordinates[0] >= region.initial[0] && m.coordinates[0] <= region.final[0]) &&
-							(m.coordinates[1] <= region.initial[1] && m.coordinates[1] >= region.final[1])) {
-							selectedMessages.push(m.message);
-						}
-					} else if (region.initial[0] > region.final[0] && region.initial[1] > region.final[1]) {
-						if ((m.coordinates[0] <= region.initial[0] && m.coordinates[0] >= region.final[0]) &&
-							(m.coordinates[1] <= region.initial[1] && m.coordinates[1] >= region.final[1])) {
-							selectedMessages.push(m.message);
-						}
-					} else if (region.initial[0] > region.final[0] && region.initial[1] < region.final[1]) {
-						if ((m.coordinates[0] <= region.initial[0] && m.coordinates[0] >= region.final[0]) &&
-							(m.coordinates[1] >= region.initial[1] && m.coordinates[1] <= region.final[1])) {
-							selectedMessages.push(m.message);
-						}
+				if (region.initial[0] < region.final[0] && region.initial[1] < region.final[1]) {
+					if ((m.coordinates[0] >= region.initial[0] && m.coordinates[0] <= region.final[0]) &&
+						(m.coordinates[1] >= region.initial[1] && m.coordinates[1] <= region.final[1])) {
+						selectedMessages.push(m.message);
 					}
-
-				});
-			}
-
-		}, 10000);
-
-		return selectedMessages;
-	
+				} else if (region.initial[0] < region.final[0] && region.initial[1] > region.final[1]) {
+					if ((m.coordinates[0] >= region.initial[0] && m.coordinates[0] <= region.final[0]) &&
+						(m.coordinates[1] <= region.initial[1] && m.coordinates[1] >= region.final[1])) {
+						selectedMessages.push(m.message);
+					}
+				} else if (region.initial[0] > region.final[0] && region.initial[1] > region.final[1]) {
+					if ((m.coordinates[0] <= region.initial[0] && m.coordinates[0] >= region.final[0]) &&
+						(m.coordinates[1] <= region.initial[1] && m.coordinates[1] >= region.final[1])) {
+						selectedMessages.push(m.message);
+					}
+				} else if (region.initial[0] > region.final[0] && region.initial[1] < region.final[1]) {
+					if ((m.coordinates[0] <= region.initial[0] && m.coordinates[0] >= region.final[0]) &&
+						(m.coordinates[1] >= region.initial[1] && m.coordinates[1] <= region.final[1])) {
+						selectedMessages.push(m.message);
+					}
+				}
+			});
+			callback(selectedMessages);
+		}
 	}
 
-	console.log(selectMessages());
-//	var selection = selectMessages();
-
-/*
-	// Opens long-lived channel b/w content script and extension
-	var port = chrome.runtime.connect(window.localStorage.getItem('messagebook-id'), {name: "selected-messages"});
-	port.postMessage({messages: selection});
-	port.onMessage.addListener(function(response) {
-
-  		console.log("Status: " + response.status);
-	
+	window.addEventListener('message', function(event) {
+		if (event.data.type && event.data.type == "tabber_run") {
+			console.log("Content script received: " + event.data.text);
+			selectMessages(function(selectedMessages) {
+				// TODO: Next step goes here
+				console.log(selectedMessages);
+			});
+		}
 	});
-*/
-
 }
 
 // Prepares the JS injection
 var inject = function() {
-
 	var script = document.createElement('script');
 	script.textContent = "(" + payload.toString() + ")();";
+	// script.textContent = payload.toString() + "";
 	document.head.appendChild(script);
-
 }
 
-// TO-DO: Handle on-click event for the browser action
-
+// TODO: Handle on-click event for the browser action
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-
-	if (request.message == "clicked_browser_action") {
-		console.log("User clicked browser action.");
+	if (request.message == "clicked_browser_action" && !injected) {
+		console.log("User clicked browser action for first time. Injecting stuff.");
+		injected = true;
 		inject();
 	}
-
+	window.postMessage({type: 'tabber_run', text: 'run the damn script'}, '*' );
 });
 
 ////BRAINSTORMING////
