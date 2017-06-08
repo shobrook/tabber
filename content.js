dcd6cb0f029280ed0094c4caa360bb8f4cf281de
Jonathan Shobrook <shobrookj@gmail.com>
2017-06-08 02:29:29 -0400

    Added save dialog, cleaned up a bit

--- content.js
+++ content.js
@@ -42,6 +42,7 @@ var payload = function() {
 
 				});
 			}
+
 		});
 
 		return scrapedMessages;
@@ -193,6 +194,8 @@ var payload = function() {
 				      			<option value="insights"> Insights </option>
 				    			</select>
 
+									<input type="submit" value="Save" style="width: 100%; background-color: #2C9ED4; color: white; padding: 14px 20px; margin: 8px 0; border: none; border-radius: 4px; cursor: pointer;">
+				    			<input id="cancelButton" type="button" value="Cancel" style="width: 100%; background-color: #FFF; color: #2C9ED4; padding: 14px 20px; margin: 8px 0; border-style: solid; border-color: #2C9ED4; border-radius: 4px; cursor: pointer;">
 				  			</form>`;
 
 				canvas.style = "background-color: rgba(0,0,0,.35); z-index: 2147483647; width: 100%; height: 100%; top: 0px; left: 0px; display: block; position: absolute;";
@@ -224,22 +227,46 @@ var payload = function() {
 				document.body.appendChild(saveDialog); // Prompts the "save" dialog
 
 				var saveForm = document.getElementById("saveForm");
+				var cancelForm = document.getElementById("cancelButton");
 
+				saveForm.onsubmit = function() {
+
+					var name = document.getElementById("saveForm").name.value;
+					var folder = document.getElementById("saveForm").folder.value;
+
+					// TODO: Send name + folder to background script
+					console.log(name + " " + folder);
+
+					document.body.removeChild(saveDialog);
+					document.body.removeChild(canvas);
+
+				}
+
+				cancelForm.onclick = function() {
+
+					document.body.removeChild(saveDialog);
+					document.body.removeChild(canvas);
+
+				}
 
 			});
 		}
 
 	});
+
 }
 
 // Prepares the JS injection
 var inject = function() {
+
 	var script = document.createElement('script');
 	script.textContent = "(" + payload.toString() + ")();";
 	document.head.appendChild(script);
+
 }
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+
 	if (request.message == "clicked_browser_action" && !injected) {
 		console.log("User clicked browser action for first time. Injecting stuff.");
 		injected = true;
