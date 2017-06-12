eb746514d9db80e8e249dfc135e5ef886aa55352
Jonathan Shobrook <shobrookj@gmail.com>
2017-06-11 23:55:03 -0400

    Implemented VERY basic REST API

--- server/server.py
+++ server/server.py
@@ -1,24 +1,71 @@
-from flask import Flask
-from flask import render_template
-import os
-from flask import jsonify
+import json
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-
+# TODO: Set up a messages.json file pipeline
+messages = [
+	{
+		"id": 1,
+		"type": u"folder",
+		"name": u"Protips",
+		"content": [
+			{
+				"type": u"file",
+				"name": u"Importance of Stories",
+				"content": [
+					{
+						"sender": 1,
+						"message": u"Stories from others give you world knowledge, different perspectives, and lessons to reiterate to others"
+					}
+				]
+			}
+		]
+	},
+	{
+		"id": 2,
+		"type": u"folder",
+		"name": u"App Ideas",
+		"content": []
+	},
+	{
+		"id": 3,
+		"type": u"folder",
+		"name": u"One-liners",
+		"content": []
+	}
+]
 
 # Default response; return empty string
 @app.route("/")
 def main():
-	return "Something"
+	return ""
 
-# If user inputs two points, generates trip
-# This method will be the main method. We will specify rules with more arguments later like <gas_stations> or <attractions> for custom trips
-@app.route("/level-one/<var_a>")
-def trip(var_a):
-	return "Something else"
+# Testing only
+@app.route("/tabber/api/get_messages", methods=["GET"])
+def get_messages():
+	return jsonify({"messages": messages})
 
+@app.route("/tabber/api/get_folders", methods=["GET"])
+def get_folders():
+	folders = []
+	for i in messages:
+		if i["type"] == "folder": folders.append(i["name"])
+	return jsonify({"folders": folders})
 
+@app.route("/tabber/api/add_message", methods=["POST"])
+def add_message():
+	if not request.json or not "name" in request.json:
+		abort(400)
+	message = {
+		"type": u"file",
+		"name": request.json["name"],
+		"content": request.json["content"]
+	}
+	for i in messages:
+		if i["type"] == "folder" and i["name"] == request.json["folder"]:
+			i["content"].append(message)
+	return jsonify({"folder": request.json["folder"], "message": message}), 201
 
 if __name__ == "__main__":
 	app.run(debug=True)
