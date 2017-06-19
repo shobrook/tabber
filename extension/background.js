7e5563f106e9984d7033fbc2ea032eea000ad321
Jonathan Shobrook <shobrookj@gmail.com>
2017-06-19 16:18:49 -0400

    Cleaned and refactored; everything works now

--- extension/background.js
+++ extension/background.js
@@ -1,144 +1,66 @@
-// GLOBALS
+/* GLOBALS */
 
-/* Nothin' here yet */
-
-// MAIN
-
-// TODO: If user exits signup dialog, prompt dialog whenever user clicks browser action
-
-// Listens for credentials and registers new user (or authenticates returning user)
-chrome.runtime.onConnect.addListener(function(port) {
-	// Handles new user
-	if (port.name == "signup") {
-		port.onMessage.addListener(function(msg) {
-			var checkEmail = new XMLHttpRequest();
-			checkEmail.open("POST", 'http://localhost:5000/tabber/api/validate_user', true);
-			checkEmail.setRequestHeader("Content-type", "application/json");
-			checkEmail.onreadystatechange = function() {
-				if (checkEmail.readyState == XMLHttpRequest.DONE && checkEmail.status == 200) { // readyState == 4
-					if (checkEmail.responseText) {
-						// TODO: Send message to login.js confirming email invalidity
-					} else if (checkEmail.responseText == false) {
-						console.log("Email is valid. Registering user.")
-
-						// TODO: Send message to login.js confirming email validity
-
-						// Pulls current user's unique authToken
-						chrome.identity.getAuthToken({interactive: true}, function(token) {
-							if (chrome.runtime.lastError) {
-								console.log("Error retrieving authToken: " + chrome.runtime.lastError.message);
-								return;
-							}
-
-							var addUser = new XMLHttpRequest();
-							addUser.open("POST", 'http://localhost:5000/tabber/api/new_user', true);
-							addUser.setRequestHeader("Content-type", "application/json");
-							addUser.onreadystatechange = function() {
-								if (addUser.readyState == XMLHttpRequest.DONE && addUser.status == 200) { // readyState == 4
-									console.log("User has successfully registered.");
-									// TODO: Prompt the tutorial screen (send message to tutorial.js)
-								}
-							}
-							addUser.send(JSON.stringify({authToken: [token], email: msg.email, password: msg.password})); // Adds new user to DB
-						});
-					}
-				}
-			}
-			checkEmail.send(JSON.stringify({"email": msg.email})); // Checks if email is already in use
-		});
-	} else if (port.name == "login") { // Handles returning user
-		port.onMessage.addListener(function(msg) {
-			var checkCreds = new XMLHttpRequest();
-			checkCreds.open("POST", 'http://localhost:5000/tabber/api/validate_user', true);
-			checkCreds.setRequestHeader("Content-type", "application/json");
-			checkCreds.onreadystatechange = function() {
-				if (checkCreds.responseText) {
-					// TODO: Send message to login.js confirming invalid credentials
-				} else if (checkCreds.responseText == false) {
-					console.log("Credentials are valid. Logging in user.");
-
-					chrome.identity.getAuthToken({interactive: true}, function(token) {
-						if (chrome.runtime.lastError) {
-							console.log("Error retrieving authToken: " + chrome.runtime.lastError.message);
-							return;
-						}
-
-						var updateToken = new XMLHttpRequest();
-						updateToken.open("POST", 'http://localhost:5000/tabber/api/update_user', true);
-						updateToken.setRequestHeader("Content-type", "application/json");
-						updateToken.onreadystatechange = function() {
-							if (updateToken.readyState == XMLHttpRequest.DONE && updateToken.status == 200) { // readyState == 4
-								console.log("Successfully updated user's authentication tokens.");
-								// TODO: Send success confirmation back to login.js
-							}
-						}
-						updateToken.send(JSON.stringify({authToken: token, email: msg.email, password: msg.password})); // Updates user's auth tokens
-					});
-				}
-			}
-		});
-	} else if (port.name == "saved-messages") { // Handles selected messages
-		port.onMessage.addListener(function(msg) {
-			chrome.identity.getAuthToken({interactive: true}, function(token) {
-				if (chrome.runtime.lastError) {
-					console.log("Error retrieving authToken: " + chrome.runtime.lastError.message);
-					return;
-				}
-
-				var addConversation = new XMLHttpRequest();
-				addConversation.open("POST", 'http://localhost:5000/tabber/api/add_conversation', true);
-				addConversation.setRequestHeader("Content-type", "application/json");
-				addConversation.onreadystatechange = function() {
-					if (addConversation.readyState == XMLHttpRequest.DONE && addConversation.status == 200) { // readyState == 4
-						console.log("Successfully added selected conversation.");
-						// TODO: Send success confirmation back to save.js
-					}
-				}
-				updateToken.send(JSON.stringify({authToken: token, "name": msg.name, "folder": msg.folder, "messages": msg.messages})); // Adds conversation to specified folder
-			});
-		});
+// Pulls user's unique authentication token
+var oauth;
+chrome.identity.getAuthToken({interactive: true}, function(token) {
+	if (chrome.runtime.lastError) {
+		console.log("Error retrieving authToken: " + chrome.runtime.lastError.message);
+		return;
 	}
+	oauth = token;
 });
 
-// Prompts the signup/login dialog on first install; logs on update
+// REST API endpoints
+const NEW_USER = "http://localhost:5000/tabber/api/new_user";
+const UPDATE_USER = "http://localhost:5000/tabber/api/update_user";
+const VALIDATE_USER = "http://localhost:5000/tabber/api/validate_user";
+const ADD_CONVERSATION = "http://localhost:5000/tabber/api/add_conversation";
+const GET_FOLDERS = "http://localhost:5000/tabber/api/get_folders";
+
+// Creates an HTTP POST request
+var POST = function(url, payload, callback) {
+	var xhr = new XMLHttpRequest();
+	xhr.open("POST", url, true);
+	xhr.setRequestHeader("Content-type", "application/json");
+	xhr.onreadystatechange = function() {
+		if (xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200) // readyState == 4
+			callback(xhr.responseText);
+	}
+	xhr.send(JSON.stringify(payload));
+}
+
+/* MAIN */
+
+// TODO: If user exits signup dialog, prompt dialog whenever user clicks browser action
+// TODO: Prompt login dialog only if on new device
+
+// Prompts the onboarding dialog on "first-install"; provides handler for extension updates
 chrome.runtime.onInstalled.addListener(function(details) {
 	if (details.reason == "install") {
-		console.log("User's first time installing tabber.");
+		console.log("User has installed tabber for the first time on this device.");
 		chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
 			var activeTab = tabs[0];
 			chrome.tabs.sendMessage(activeTab.id, {"message": "first_install"});
-		})
+		});
 	} else if (details.reason == "update") {
 		var thisVersion = chrome.runtime.getManifest().version;
 		console.log("Updated from " + details.previousVersion + " to " + thisVersion + " :)");
 	}
 });
 
-// Called when a user clicks the browser action
+// Passes user's folder references to save.js and prompts "select messages" dialog on click of browser action
 chrome.browserAction.onClicked.addListener(function(tab) {
-	chrome.identity.getAuthToken({interactive: true}, function(token) {
-		if (chrome.runtime.lastError) {
-			console.log(chrome.runtime.lastError.message);
-			return;
-	  }
-
-		var getFolders = new XMLHttpRequest();
-		getFolders.open("POST", 'http://localhost:5000/tabber/api/get_folders', true);
-		getFolders.setRequestHeader("Content-type", "application/json");
-		getFolders.onreadystatechange = function() {
-			if (getFolders.readyState == XMLHttpRequest.DONE && getFolders.status == 200) { // readyState == 4
-				chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-					var activeTab = tabs[0];
-					chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action", "folders": JSON.parse(getFolders.responseText).folders});
-					console.log(JSON.parse(getFolders.responseText).folders);
-				});
-			}
+	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+		var saveDialog = function(folders) {
+			var activeTab = tabs[0];
+			chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action", "folders": JSON.parse(folders).folders});
+			console.log("Passed folder references to save dialog.");
 		}
-		getFolders.send(JSON.stringify({"authToken": token}));
+		POST(GET_FOLDERS, {"authToken": oauth}, saveDialog);
 	});
 });
 
-// Creates and handles "Find Messages" in context menu
+// Creates "Find Messages" in context menu and prompts file manager
 chrome.contextMenus.create({
 	title: "Find Messages",
 	contexts: ["browser_action"],
@@ -148,4 +70,51 @@ chrome.contextMenus.create({
 			chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_find_messages"});
 		});
 	}
+});
+
+// Listens for messages passed from all content scripts
+chrome.runtime.onConnect.addListener(function(port) {
+	port.onMessage.addListener(function(msg) {
+		if (port.name == "register") {
+			var addUser = function(emailCheck) {
+				if (JSON.parse(emailCheck).valid) {
+					console.log("Email is valid. Registering user.");
+					// TODO: Send message to login.js confirming email validity
+					var onBoarding = function(userID) {
+						console.log("User has successfully registered.");
+						// TODO: Prompt the tutorial screen (send message to tutorial.js)
+					}
+					POST(NEW_USER, {"authToken": oauth, "email": msg.email, "password": msg.password}, onBoarding);
+				} else if (!(JSON.parse(emailCheck).valid)) {
+					console.log("Email is invalid. Try again.");
+					// TODO: Send message to login.js confirming email invalidity
+				}
+			}
+			POST(VALIDATE_USER, {"email": msg.email}, addUser);
+		} else if (port.name == "login") {
+			var updateTokens = function(credCheck) {
+				if (JSON.parse(credCheck).valid) {
+					console.log("Valid credentials. Logging in user.");
+					var deviceCheck = function(device) {
+						if (JSON.parse(device).updated)
+							console.log("User is logging in via a new device. Successfully updated user's authentication tokens.");
+						else if (!(JSON.parse(device).updated))
+							console.log("User is logging in via a known device.");
+						// TODO: Send success confirmation back to login.js
+					}
+					POST(UPDATE_USER, {"authToken": oauth, "email": msg.email, "password": msg.password}, deviceCheck);
+				} else if (!(JSON.parse(credCheck).valid)) {
+					console.log("Invalid credentials.");
+					// TODO: Send message to login.js confirming invalid credentials
+				}
+			}
+			POST(VALIDATE_USER, {"email": msg.email, "password": msg.password}, updateTokens);
+		} else if (port.name == "saved-messages") {
+			var convoCheck = function(convoID) {
+				console.log("Successfully added selected conversation.");
+				// TODO: Send success confirmation back to save.js
+			}
+			POST(ADD_CONVERSATION, {"authToken": oauth, "name": msg.name, "folder": msg.folder, "messages": msg.messages}, convoCheck);
+		}
+	});
 });
