ab4f49ff0077be5bcdeea788058744da84e97afe
Jonathan Shobrook <shobrookj@gmail.com>
2017-06-24 21:53:16 -0400

    Stylized the "Find Messages" protip

--- extension/content/onboarding.js
+++ extension/content/onboarding.js
@@ -1,60 +1,76 @@
+// TODO: Add a protip for seleting / saving conversations
+
 /* GLOBALS */
 
-injectedOnboarding = false;
+// Nothin' here yet
 
 /* MAIN */
 
-var onboardingPayload = function() {
+var protipPayload = function() {
 	console.log("Prompting post-save tutorial.");
 
 	var canvas = document.createElement('div');
-	var tutorial = document.createElement("div");
+	var protip = document.createElement("div");
 
-	var content_defs = `<div id="tooltip">
-  								      <h3> You just saved your first conversation! </h3>
-  									    <p> Right-click the extension icon and select "Find Messages" to access your saved conversations. </p>
-  									    <input id="gotIt" type="submit" value="Got it!">
-									    </div>`;
+	// TODO: Update font-family with Neuzeit S LT Std
+	// TODO: Polish the text padding and overflow behavior
+	// TODO: Add in a picture of the extension icon
+	var tip_defs = `<div id="contents" style="text-align: center; color: #7D848E; font-family: Helvetica; padding-top: 4%;">
+										<h3 style="font-size: 15px; font-weight: 600;"> You just saved your first conversation! </h3>
+										<p style="font-weight: 400; font-size: 14px; width: 79%; margin-left: auto; margin-right: auto;"> Right-click the extension icon and select "Find Messages" to access your saved conversations. </p>
+										<input id="got-it" type="submit" value="Got it!" style="border: none; color: #FFFFFF; background-color: #2C9ED4; text-align: center; font-family: Helvetica; font-size: 14px; font-weight: 400; padding: 12px 10%; border-radius: 12px 2px 12px 2px; cursor: pointer; margin-top: 3.05%;">
+									</div>`;
 
 	canvas.style = "background-color: rgba(0,0,0,.35); z-index: 2147483647; width: 100%; height: 100%; top: 0px; left: 0px; display: block; position: absolute;";
 
-	tutorial.style.position = "fixed";
-	tutorial.style.width = "50%";
-	tutorial.style.height = "400px";
-	tutorial.style.top = "15%";
-	tutorial.style.left = "25%";
-	tutorial.style.borderRadius = "5px";
-	tutorial.style.padding = "20px";
-	tutorial.style.backgroundColor = "#FFFFFF";
-	//tutorial.style.boxShadow = "0px 1px 4px #000000";
-	tutorial.style.zIndex = "2147483647";
+	protip.style.position = "fixed";
+	protip.style.width = "27%";
+	protip.style.height = "190px";
+	protip.style.top = "50%";
+	protip.style.left = "36.5%";
+	protip.style.transform = "translateY(-50%)";
+	protip.style.borderRadius = "10px";
+	protip.style.backgroundColor = "#FFFFFF";
+	protip.style.zIndex = "2147483647";
 
-	tutorial.innerHTML = content_defs;
+	protip.innerHTML = tip_defs;
 
 	document.body.appendChild(canvas); // Imposes a low-opacity "canvas" on entire page
-	document.body.appendChild(tutorial); // Prompts the "sign up" dialog
+	document.body.appendChild(protip); // Prompts the "sign up" dialog
 
-	var confirm = document.getElementById("gotIt");
+	var confirm = document.getElementById("got-it");
+
+	confirm.onmouseover = function() {
+		this.style.backgroundColor = "#52AFDB";
+	}
 
 	confirm.onsubmit = function() {
+		document.body.removeChild(protip);
+		document.body.removeChild(canvas);
+	}
+
+	canvas.onclick = function() {
 		document.body.removeChild(tutorial);
 		document.body.removeChild(canvas);
+
+		// TODO: When canvas is clicked, the protip should still display each time a conversation
+		// is saved (until "Got It" is clicked)
 	}
 
 	console.log("Displayed an onboarding tooltip.");
 }
 
 // Prepares the JS injection
-var onboardingInject = function() {
+var protipInject = function() {
 	var script = document.createElement('script');
-	script.textContent = "(" + onboardingPayload.toString() + ")();";
+	script.textContent = "(" + protipPayload.toString() + ")();";
 	document.head.appendChild(script);
 }
 
-// Listens for the "extension install" event
+// Listens for the first "saved messages" event
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 	if (request.message == "first_save") {
 		console.log("User has saved a conversation for the first time.");
-		onboardingInject();
+		protipInject();
 	}
 });
