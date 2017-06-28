b81a520974e5a87fd0d1e58476d10d81fec978d5
Jonathan Shobrook <shobrookj@gmail.com>
2017-06-27 23:55:40 -0400

    Added reprompt support for signup dialog and onboarding

--- extension/background.js
+++ extension/background.js
@@ -1,8 +1,10 @@
+// TODO: Set up the content script for Invite a Friend
+
 /* GLOBALS */
 
 // Pulls user's unique authentication token; TODO: Needs to be fixed!
 var oauth;
-chrome.identity.getAuthToken({interactive: false}, function(token) {
+chrome.identity.getAuthToken({interactive: true}, function(token) {
 	if (chrome.runtime.lastError) {
 		console.log("Error retrieving authToken: " + chrome.runtime.lastError.message);
 		return;
@@ -33,14 +35,14 @@ var POST = function(url, payload, callback) {
 	xhr.send(JSON.stringify(payload));
 }
 
-// Boolean for whether a user is using the extension for the first time
-var firstInstall;
+// Boolean for whether onboarding should be initiated
+var onboarding;
+// Boolean for whether signup should be initiated
+var signup = true; // NOTE: Set as "true" for testing only
 
 /* MAIN */
 
-// TODO: If user exits signup dialog, prompt dialog whenever user clicks browser action
-
-// Prompts the onboarding dialog on "first-install"; provides handler for extension updates
+// Prompts the sign-up dialog on "first-install"; provides handler for extension updates
 chrome.runtime.onInstalled.addListener(function(details) {
 	if (details.reason == "install") {
 		console.log("User has installed tabber for the first time on this device.");
@@ -48,6 +50,7 @@ chrome.runtime.onInstalled.addListener(function(details) {
 			var activeTab = tabs[0];
 			chrome.tabs.sendMessage(activeTab.id, {"message": "first_install"});
 		});
+		signup = true;
 	} else if (details.reason == "update") {
 		var thisVersion = chrome.runtime.getManifest().version;
 		console.log("Updated from " + details.previousVersion + " to " + thisVersion + " :)");
@@ -55,21 +58,22 @@ chrome.runtime.onInstalled.addListener(function(details) {
 });
 
 // Passes user's folder references to save.js and prompts "select messages" dialog on click of browser action
-// NOTE: Toggle the two sections to assign browser action click to either login or message selection
 chrome.browserAction.onClicked.addListener(function(tab) {
-
-	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-		var saveDialog = function(folders) {
+	if (signup) {
+		chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
 			var activeTab = tabs[0];
-			chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action", "folders": JSON.parse(folders).folders});
-			console.log("Passed folder references to save dialog.");
-		}
-		POST(GET_FOLDERS, {"authToken": oauth}, saveDialog);
-	});
-	// chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-	// 	var activeTab = tabs[0];
-	// 	chrome.tabs.sendMessage(activeTab.id, {"message": "first_install"});
-	// });
+			chrome.tabs.sendMessage(activeTab.id, {"message": "first_install"});
+		});
+	} else {
+		chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+			var saveDialog = function(folders) {
+				var activeTab = tabs[0];
+				chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action", "folders": JSON.parse(folders).folders});
+				console.log("Passed folder references to save dialog.");
+			}
+			POST(GET_FOLDERS, {"authToken": oauth}, saveDialog); // TODO: Only pull the top 10 most popular folders
+		});
+	}
 });
 
 // Creates "Find Messages" in context menu and prompts file manager
@@ -77,10 +81,18 @@ chrome.contextMenus.create({
 	title: "Find Messages",
 	contexts: ["browser_action"],
 	onclick: function () {
-		chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-			var activeTab = tabs[0];
-			chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_find_messages"});
-		});
+		if (!signup) {
+			chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+				var activeTab = tabs[0];
+				chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_find_messages"});
+			});
+			onboarding = false;
+		} else {
+			chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+				var activeTab = tabs[0];
+				chrome.tabs.sendMessage(activeTab.id, {"message": "first_install"});
+			});
+		}
 	}
 });
 
@@ -97,26 +109,28 @@ chrome.contextMenus.create({
 	}
 });
 
-// Content scripts --> here --> server.py
+// Content scripts --> here --> server.py --> here --> content scripts
 chrome.runtime.onConnect.addListener(function(port) {
 	port.onMessage.addListener(function(msg) {
 		if (port.name == "register") {
 			var addUser = function(emailCheck) {
 				if (JSON.parse(emailCheck).valid) {
 					console.log("Email is valid. Registering user.");
-					port.postMessage({validEmail: true}); // Pass valid email
 					var onBoarding = function(userID) {
 						console.log("User has successfully registered.");
-						port.postMessage({registered: true}); // Pass successful registration
-						firstInstall = true;
+						port.postMessage({registered: true});
+						onboarding = true;
+						signup = false;
 					}
 					POST(NEW_USER, {"authToken": oauth, "email": msg.email, "password": msg.password}, onBoarding);
 				} else if (!(JSON.parse(emailCheck).valid)) {
 					console.log("Email is invalid. Try again.");
-					port.postMessage({validEmail: false}); // Pass invalid email
+					port.postMessage({registered: false});
 				}
 			}
 			POST(VALIDATE_USER, {"email": msg.email}, addUser);
+			// TODO: Validate that email has a proper domain
+			// TODO: Compartmentalize all validation (valid email provider, email already in use) in server.py
 		}
 		else if (port.name == "login") {
 			var updateTokens = function(credCheck) {
@@ -127,26 +141,27 @@ chrome.runtime.onConnect.addListener(function(port) {
 							console.log("User is logging in via a new device. Successfully updated user's authentication tokens.");
 						else if (!(JSON.parse(device).updated))
 							console.log("User is logging in via a known device.");
-						// TODO: Send success confirmation back to login.js
+						port.postMessage({loggedIn: true});
 					}
 					POST(UPDATE_USER, {"authToken": oauth, "email": msg.email, "password": msg.password}, deviceCheck);
 				} else if (!(JSON.parse(credCheck).valid)) {
 					console.log("Invalid credentials.");
-					// TODO: Send message to login.js confirming invalid credentials
+					port.postMessage({loggedIn: false});
 				}
 			}
 			POST(VALIDATE_USER, {"email": msg.email, "password": msg.password}, updateTokens);
 		}
-		else if (port.name == "saved-messages") {
+		else if (port.name == "conversations") {
 			var convoCheck = function(convoID) {
 				console.log("Successfully added selected conversation.");
-				if (firstInstall) {
+				if (onboarding) {
+					console.log("User's first time saving a conversation.");
 					chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
 						var activeTab = tabs[0];
 						chrome.tabs.sendMessage(activeTab.id, {"message": "first_save"});
 					});
-					firstInstall = false;
 				}
+				port.postMessage({saved: true});
 				// TODO: Send success confirmation back to save.js
 			}
 			POST(ADD_CONVERSATION, {"authToken": oauth, "name": msg.name, "folder": msg.folder, "messages": msg.messages}, convoCheck);
@@ -178,6 +193,16 @@ chrome.runtime.onConnect.addListener(function(port) {
 				}
 				POST(RENAME_FOLDER, {"authToken": oauth, "name": msg.name, "newName": msg.newName}, renamedFolder);
 			});
+		} else if (port.name == "onboarding") {
+			if (msg.submitted)
+				onboarding = false;
+			else if (!(msg.submitted))
+				onboarding = true;
+		} else if (port.name == "registered") {
+			if (msg.registered)
+				signup = true;
+			else if (!(msg.registered))
+				signup = false;
 		}
 	});
 });
