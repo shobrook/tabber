e0b6d21570d44800531b3194743843a5a7cd1268
Jonathan Shobrook <shobrookj@gmail.com>
2017-06-28 23:49:54 -0400

    Added fully stylized signup and login dialog

--- extension/content/register.js
+++ extension/content/register.js
@@ -1,5 +1,3 @@
-// TODO: Stylize the sign-up and login dialogs
-
 /* GLOBALS */
 
 var registerPort = chrome.runtime.connect(window.localStorage.getItem('tabber-id'), {name: "register"});
@@ -16,28 +14,37 @@ var registerPayload = function() {
 	var canvas = document.createElement('div');
 	var signUpDialog = document.createElement("div");
 
-	var formDefs = `<div id="signUpWrapper">
-      							<div id="dialogTabs">
-        							<h3 id="signUpTab">SIGN UP</h3>
-        							<h3 id="loginTab">LOGIN</h3>
-      							</div><!--dialogTabs-->
+	var formDefs = `<div id="registerWrapper" style="margin: auto;">
+							      <div class="dialogTabs">
+							        <h3 id="signUpTab">SIGN UP</h3>
+							        <h3 id="loginTab">LOGIN</h3>
+							      </div><!--dialogTabs-->
 
-      							<hr id="divisor">
-      							<hr id="selector">
+							      <hr id="divisor">
+							      <hr id="selector">
 
-      							<div id="tabContent">
-        							<div id="signUpTabContent" class="active">
-          							<form id="signUpForm">
-            							<input type="email" class="inputFields" name="tabberEmail" id="tabberEmail" autocomplete="off" placeholder="Email Address">
-            							<input type="password" class="inputFields" name="tabberPass" id="tabberPass" autocomplete="off" placeholder="Password">
-            							<input id="signUpButton" type="submit" value="Get Started">
-            							<div id="passwordHelpText">
-              							<p>Passwords must be at least 6 characters long.</p>
-            							</div><!--passwordHelpText-->
-          							</form><!--signUpForm-->
-        							</div><!--signUpTabContent-->
-      							</div><!--tabContent-->
-    							</div><!--signUpWrapper-->`
+							      <div id="tabContent">
+							        <div id="signUpTabContent">
+							          <form id="signUpForm">
+							            <input type="email" class="inputFields" id="tabberEmail" autocomplete="false" placeholder="Email Address">
+							            <input type="password" class="inputFields" id="tabberPass" autocomplete="false" placeholder="Password">
+							            <input id="signUpButton" class="signUpLoginButton" type="submit" value="Get Started">
+							            <div id="passwordHelpText">
+							              <p>Passwords must be at least 6 characters long.</p>
+							            </div><!--passwordHelpText-->
+							          </form><!--signUpForm-->
+							        </div><!--signUpTabContent-->
+							        <div id="loginTabContent" style="display: none;">
+							          <form id="loginForm">
+							            <input type="email" class="inputFields" id="tabberEmail" autocomplete="false" placeholder="Email Address">
+							            <input type="password" class="inputFields" id="tabberPass" autocomplete="false" placeholder="Password">
+							            <input id="loginButton" type="submit" class="signUpLoginButton" value="Continue">
+							            <div id="forgotYourPassword">
+							              <p>Forgot your password?</p>
+							            </div><!--forgotYourPassword-->
+							          </form><!--loginForm-->
+							      </div><!--tabContent-->
+							    </div><!--registerWrapper-->`;
 
 	canvas.style.backgroundColor = "rgba(0,0,0,.35)";
 	canvas.style.zIndex = "2147483647";
@@ -61,147 +68,187 @@ var registerPayload = function() {
 
 	signUpDialog.innerHTML = formDefs;
 
-	document.getElementsByTagName('style')[0].innerHTML = `#signUpWrapper {
-																												   margin: auto;
-																												 }
+	document.getElementsByTagName('style')[0].innerHTML = `.dialogTabs {
+																												  overflow: hidden;
+																												  font-family: Helvetica;
+																												  margin-left: 40px;
+																												}
 
-																												 #dialogTabs {
-																												   overflow: hidden;
-																													 font-family: Helvetica;
-																													 margin-left: 40px;
-																												 }
+																												#signUpTab {
+																												  float: left;
+																												  width: 239px;
+																												  padding: 1.8em 0;
+																												  display: block;
+																												  text-align: center;
+																												  font-size: 13px;
+																												  font-weight: 600;
+																												  color: rgb(44,158,212);
+																												  cursor: pointer;
+																												}
 
-																												 #signUpTab {
-																												   float: left;
-																													 width: 239px;
-																													 padding: 0.75em 0;
-																													 display: block;
-																													 text-align: center;
-																													 font-size: 13px;
-																													 font-weight: 600;
-																													 color: rgb(44,158,212);
-																													 cursor: pointer;
-																												 }
+																												#loginTab {
+																												  float: left;
+																												  width: 239px;
+																												  padding: 1.8em 0;
+																												  display: block;
+																												  text-align: center;
+																												  font-size: 13px;
+																												  font-weight: 600;
+																												  color: rgb(195,208,225);
+																												  cursor: pointer;
+																												}
 
-																												 #loginTab {
-																												   float: left;
-																													 width: 239px;
-																													 padding: 0.75em 0;
-																													 display: block;
-																													 text-align: center;
-																													 font-size: 13px;
-																													 font-weight: 600;
-																													 color: rgb(195,208,225);
-																													 cursor: pointer;
-																												 }
+																												#signUpTab:hover {
+																												  color: rgb(44,158,212);
+																												}
 
-																												 #divisor {
-																												   background-color: #F5F7F9;
-																													 height: 1px;
-																													 border: none;
-																													 margin-top: -3px;
-																												 }
+																												#loginTab:hover {
+																												  color: rgb(44,158,212);
+																												}
 
-																												 #selector {
-																												   background-color: #2C9ED4;
-																													 height: 1px;
-																													 border: none;
-																													 width: 239px;
-																													 margin-top: -9px;
-																													 margin-left: 40px;
-																													 transition: 1s;
-																												 }
+																												#divisor {
+																												  background-color: #F5F7F9;
+																												  height: 1px;
+																												  border: none;
+																												  margin-top: -3px;
+																												}
 
-																												 #tabContent {
-																												   position: relative;
-																													 top: 30px;
-																													 left: 40px;
-																												 }
+																												#selector {
+																												  background-color: #2C9ED4;
+																												  height: 1px;
+																												  border: none;
+																												  width: 239px;
+																												  margin-top: -7px;
+																												  margin-left: 40px;
+																												}
 
-																												 .inputFields {
-																												   box-sizing: border-box;
-																													 width: 478px;
-																													 padding: 14px 20px;
-																													 outline: none;
-																													 display: inline-block;
-																													 margin: 0 0 15px 0;
-																													 border: none;
-																													 border-radius: 1px;
-																													 box-shadow: 0px 1px 3px #D9D9D9;
-																													 color: #7D858E;
-																													 font-family: Helvetica;
-																													 font-weight: 400;
-																													 font-size: 13px;
-																												 }
+																												#tabContent {
+																												  position: relative;
+																												  top: 30px;
+																												  left: 40px;
+																												}
 
-																												 #tabberEmail::-webkit-input-placeholder {
-																												   color: #CDD8E6;
-																												 }
+																												.inputFields {
+																												  box-sizing: border-box;
+																												  width: 478px;
+																												  padding: 14px 20px;
+																												  outline: none;
+																												  display: inline-block;
+																												  margin: 0 0 15px 0;
+																												  border: none;
+																												  border-radius: 1px;
+																												  box-shadow: 0px 1px 3px #D9D9D9;
+																												  color: #7D858E;
+																												  font-family: Helvetica;
+																												  font-weight: 400;
+																												  font-size: 13px;
+																												}
 
-																												 #tabberPass::-webkit-input-placeholder {
-																												   color: #CDD8E6;
-																												 }
+																												#tabberEmail::-webkit-input-placeholder {
+																												  color: #CDD8E6;
+																												}
 
-																												 #passwordHelpText {
-																												   position: relative;
-																													 top: -54px;
-																													 display: inline-block;
-																													 font-family: Helvetica;
-																													 font-weight: 500;
-																													 font-size: 10px;
-																													 color: #CDD8E6;
-																												 }
+																												#tabberPass::-webkit-input-placeholder {
+																												  color: #CDD8E6;
+																												}
 
-																												 #signUpButton {
-																												   position: relative;
-																													 background-color: #2C9ED4;
-																													 text-align: center;
-																													 width: 135px;
-																													 height: 42px;
-																													 border: none;
-																													 color: #FFFFFF;
-																													 font-family: Helvetica;
-																													 font-weight: 600;
-																													 font-size: 13px;
-																													 cursor: pointer;
-																													 border-radius: 10px 1px 10px 1px;
-																													 margin-left: 343px;
-																													 margin-top: 0px;
-																													 float: left;
-																													 overflow: hidden;
-																													 display: inline-block;
-																													 border: none;
-																													 outline: none;
-																												 }`;
+																												#passwordHelpText {
+																												  position: relative;
+																												  top: -53px;
+																												  display: inline-block;
+																												  font-family: Helvetica;
+																												  font-weight: 500;
+																												  font-size: 11px;
+																												  color: rgb(44,158,212);
+																												}
+
+																												#forgotYourPassword {
+																												  position: relative;
+																												  top: -53px;
+																												  display: inline-block;
+																												  font-family: Helvetica;
+																												  font-weight: 500;
+																												  font-size: 11px;
+																												  color: rgb(44,158,212);
+																												  text-decoration: underline;
+																												  cursor: pointer;
+																												}
+
+																												.signUpLoginButton {
+																												  position: relative;
+																												  background-color: #2C9ED4;
+																												  text-align: center;
+																												  width: 135px;
+																												  height: 42px;
+																												  border: none;
+																												  color: #FFFFFF;
+																												  font-family: Helvetica;
+																												  font-weight: 600;
+																												  font-size: 13px;
+																												  cursor: pointer;
+																												  border-radius: 10px 1px 10px 1px;
+																												  margin-left: 343px;
+																												  margin-top: 0px;
+																												  float: left;
+																												  overflow: hidden;
+																												  display: inline-block;
+																												  border: none;
+																												  outline: none;
+																												}
+
+																												.signUpLoginButton:hover {
+																												  background-color: rgb(101,184,203);
+																												}`;
 
 	document.body.appendChild(canvas); // Imposes a low-opacity "canvas" on entire page
 	document.body.appendChild(signUpDialog); // Prompts the "sign up" dialog
-
-	var signUp = document.getElementById("signUpForm");
-	//var login = document.getElementById("loginLink");
-	// TODO: Implement the login page
 
 	canvas.onclick = function() {
 		window.postMessage({type: "registered", value: false}, '*');
 		document.body.removeChild(signUpDialog);
 		document.body.removeChild(canvas);
+		console.log("User clicked canvas.");
 	}
 
-	signUp.onsubmit = function() {
+	var signUpTab = document.getElementById("signUpTab");
+	var loginTab = document.getElementById("loginTab");
+
+	var loginTabContent = document.getElementById("loginTabContent");
+	var signUpTabContent = document.getElementById("signUpTabContent");
+	var selector = document.getElementById("selector");
+
+	loginTab.onclick = function() {
+	  signUpTab.style.color = "rgb(195,208,225)";
+	  loginTab.style.color = "rgb(44,158,212)";
+	  selector.style.marginLeft = "279px";
+	  signUpTabContent.style.display = "none";
+	  loginTabContent.style.display = "initial";
+	}
+
+	signUpTab.onclick = function() {
+	  signUpTab.style.color = "rgb(44,158,212)";
+	  loginTab.style.color = "rgb(195,208,225)";
+	  selector.style.marginLeft = "40px";
+	  loginTabContent.style.display = "none";
+	  signUpTabContent.style.display = "initial";
+	}
+
+	var signUpForm = document.getElementById("signUpForm");
+	var loginForm = document.getElementById("loginForm");
+
+	signUpForm.onsubmit = function() {
 		var email = (this).tabberEmail.value;
 		var password = (this).tabberPass.value;
 
 		window.postMessage({type: "signup_credentials", text: {"email": email, "password": password}}, '*');
 	}
 
-/*
-	login.onclick = function() {
-		var email = signUp.tabberEmail.value;
-		var password = signUp.tabberPass.value;
+	loginForm.onclick = function() {
+		var email = signUpForm.tabberEmail.value;
+		var password = signUpForm.tabberPass.value;
 
 		window.postMessage({type: "login_credentials", text: {"email": email, "password": password}}, '*');
 	}
-*/
 
 	window.addEventListener('message', function(event) {
 		if (event.data.type == "signUpValidation" && event.data.value) {
@@ -209,14 +256,14 @@ var registerPayload = function() {
 			document.body.removeChild(canvas);
 			console.log("User successfully registered.");
 		} else if (event.data.type == "signUpValidation" && !(event.data.value)) { // TODO: Determine if email is already taken or invalid
-			signUp.reset();
+			signUpForm.reset();
 			console.log("User tried signing up with invalid email.");
 		} else if (event.data.type == "loginValidation" && event.data.value) {
 			document.body.removeChild(signUpDialog);
 			document.body.removeChild(canvas);
 			console.log("User successfully logged in.");
 		} else if (event.data.type == "loginValidation" && !(event.data.value)) {
-			signUp.reset();
+			signUpForm.reset();
 			console.log("User inputted incorrect login credentials.");
 		}
 	});
@@ -258,6 +305,4 @@ registerPort.onMessage.addListener(function(msg) {
 loginPort.onMessage.addListener(function(msg) {
 	if (msg.loggedIn)
 		window.postMessage({type: "loginValidation", value: msg.loggedIn}, '*');
-	else
-		window.postMessage({type: "loginValidation", value: msg.loggedIn}, '*')
 });
